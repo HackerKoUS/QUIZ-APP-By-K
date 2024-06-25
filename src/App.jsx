6424e3b7ca80c3a0efef5c233d8764277255984d
HackerKoUS <120322766+HackerKoUS@users.noreply.github.com>
2024-06-25 17:55:36 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Homepage from '../components/Homepage'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route,HashRouter} from 'react-router-dom'
 import Quiz from '../components/Quiz'
 import './App.css'
 import { Answer } from '../components/Answer'
@@ -7,13 +7,13 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
         <Route exact path='/' element={<Homepage/>}/>
         <Route exact path='/quiz' element={<Quiz/>}/>
         <Route exact path='/answer' element={<Answer/>}/>
       </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
